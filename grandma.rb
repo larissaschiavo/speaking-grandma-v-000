94e700e8dcfd23d70de30468706e0b4a9ef25c11
Larissa S <larissafschiavo@gmail.com>
2017-10-31 03:09:18 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,9 +5,9 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  else 
-    NO, NOT SINCE 1938!
-  
+  else
+    return "NO, NOT SINCE 1938!"
+  end 
 end
 
 # Whatever you say to grandma, she should respond with
