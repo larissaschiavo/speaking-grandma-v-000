3eb45341cc391f6abb3eb979ccf8b6b50ab143ab
Larissa S <larissafschiavo@gmail.com>
2017-10-31 03:09:04 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -5,6 +5,8 @@ def speak_to_grandma(phrase)
     return "HUH?! SPEAK UP, SONNY!"
   elsif phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  else 
+    NO, NOT SINCE 1938!
   
 end
 
